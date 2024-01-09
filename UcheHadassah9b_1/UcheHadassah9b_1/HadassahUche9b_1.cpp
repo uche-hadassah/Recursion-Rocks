5dd63ed5da02df48a3e6d45823fac156d746bd48
Hadassah Uche <uhadassah1@gmail.com>
2024-01-09 09:26:42 +0200

    All done with commenting too

--- UcheHadassah9b_1/UcheHadassah9b_1/HadassahUche9b_1.cpp
+++ UcheHadassah9b_1/UcheHadassah9b_1/HadassahUche9b_1.cpp
@@ -1,20 +1,23 @@
-/*Write a recursive function that takes a string (the message) and an integer n and prints 
-the message, n times. */
+/*Name: Uche Hadassah
+This program uses a recursive function to print a string n number of times 
+*/
 #include<iostream>
 using namespace std;
-void print(char str[], int n)
-{
-	if (n == 1)
-		cout << str;
-	else
-	{
-		cout << str << endl;
-		return print(str, n - 1);
-	}
-}
+
 int main()
 {
 	char string[] = "Hadassah is a genius";
 	int n = 5;
 	print(string, n);
+}
+
+void print(char str[], int n)
+{
+	if (n == 1)//base case
+		cout << str;
+	else
+	{
+		cout << str << endl;//output before recursive call
+		return print(str, n - 1);//recursive call
+	}
 }
