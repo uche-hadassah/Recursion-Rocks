91d51cfa5f5bfd4481eb06aae6516e55a6d021b8
Hadassah Uche <uhadassah1@gmail.com>
2024-01-09 09:43:27 +0200

    Completed and well commented now

--- UcheHadassah9b_2a/UcheHadassah9b_2a/HadassahUche9b_2a.cpp
+++ UcheHadassah9b_2a/UcheHadassah9b_2a/HadassahUche9b_2a.cpp
@@ -1,22 +1,22 @@
-/*
-a) Write a recursive function that takes as parameter a character ch and prints the 
-alphabet starting at the character ‘a’ and ending at the character ch. For example: The 
-call PrintLetters(‘k’) should output: abcdefghijk, whereas PrintLetters(‘d’) outputs: 
-abcd.
+/*Name:Uche Hadassah
+This program prints the letters of the alphabets from 'a' to a given character using a recursive function
 */
 #include <iostream>
 using namespace std;
-void PrintLetters(char ch)
-{
-	if (ch == 'a')
-		cout << ch;
-	else
-		PrintLetters(ch - 1);
-	cout << ch << ",";
-}
 
 int main()
 {
 	char alpha = 'd';
 	PrintLetters(alpha);
+}
+
+void PrintLetters(char ch)
+{
+	if (ch == 'a')//base case
+		cout << ch <<",";
+	else
+	{
+		PrintLetters(ch - 1);//Accidentally used ch-- previously
+		cout << ch << ",";
+	}
 }
