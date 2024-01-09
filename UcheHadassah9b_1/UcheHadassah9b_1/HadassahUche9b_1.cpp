186c2545c5f4f9afcf705010e795639e37529262
Hadassah Uche <uhadassah1@gmail.com>
2024-01-09 09:21:30 +0200

    Rough sketch 1

---
+++ UcheHadassah9b_1/UcheHadassah9b_1/HadassahUche9b_1.cpp
@@ -0,0 +1,17 @@
+/*Write a recursive function that takes a string (the message) and an integer n and prints 
+the message, n times. */
+#include<iostream>
+using namespace std;
+void print(char str[], int n)
+{
+	if (n == 1)
+		cout << str;
+	else
+		return print(str, n - 1);
+}
+int main()
+{
+	char string[] = "Hadassah is a genius";
+	int n = 5;
+	print(string, n);
+}
